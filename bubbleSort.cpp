7d636a91a072a6b42c2b8a5ce5229eaf7ccb2cdc
Rajnandini2004 <133628482+Rajnandini2004@users.noreply.github.com>
2025-10-05 22:28:03 +0530

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void print(int arr[], int n)
+{
+    for(int i = 0 ; i < n ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void bubbleSort(int arr[] , int n )
+{
+    for(int i = 0 ; i < n-1 ; i++)
+    {
+        for(int j = 0 ; j < n-i-1 ; j++)
+        {
+            if(arr[j] > arr[j+1]) // ye asc order me kiya he , agar desc me krna hota to < lagate
+            {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+    print(arr, n);
+}
+
+int main()
+{
+    int arr[5] = {5,4,3,2,1};
+    bubbleSort(arr, 5);
+    return 0;
+}
